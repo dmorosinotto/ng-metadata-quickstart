9ee855866c79e4819d696672820af5c088416112
Michael de Lang <kingoipo@gmail.com>
2016-04-15 22:11:30 +0200

    Merge plugins branch

---
+++ typings/tempfix-plugin-tsc-issue-6615.d.ts
@@ -0,0 +1,7 @@
+//THIS IS A TEMPORARY FIX TO AVOID TSC COMPILE ERROR TS2307 WHEN IMPORTING TEMPLATE HTML USING SYSTEMJS PLUGINS .html!
+//WAITING FOR TYPESCRIPT ISSUE #6615 TO BE MERGED https://github.com/Microsoft/TypeScript/issues/6615
+
+declare module 'app/components/my-app.html!' { //MUST USE FULLPATH BECOUSE TS DON'T ALLOW DECLARE MODULE NAME RELATIVE './..'
+    const template: string;
+    export default template;
+}
