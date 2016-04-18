a9b9b8218e56282d71ca903bdf60723ed33ef70b
Daniele Morosinotto <d.morosinotto@icloud.com>
2016-04-18 15:58:59 +0200

    bump ver 1.1.0: add fix for ngMetadata 1.5.2 + extract systemjs.conf.js + update README and tslint rule

--- app/components/my-app.component.ts
+++ app/components/my-app.component.ts
@@ -1,28 +1,28 @@
 import {Component, Inject} from "ng-metadata/core";
 import {QuestionSvc} from "../services/question.service";
-import html from "app/components/my-app.html!"; //LOADING TEMPLATE HTML FROM EXTERNAL FILE WITH .html! SYSTEMJS PLAGIN
-//TO AVOID TS2307 ERROR YOU MUST USE ABSOLUTE PATH AND DECLARE MODULE IN    typings/tempfix-plugin-tsc-issue-6615.d.ts
+import html from "app/components/my-app.html!"; // LOADING TEMPLATE HTML FROM EXTERNAL FILE WITH .html! SYSTEMJS PLAGIN
+// TO AVOID TS2307 ERROR YOU MUST USE ABSOLUTE PATH AND DECLARE MODULE IN    typings/tempfix-plugin-tsc-issue-6615.d.ts
 
 @Component({
     selector: "my-app",
-    template: html //I PREFER INLINE TEMPLATE, BUT YOU CAN USE SYSTEMJS PLUGIN .html! TO LOAD EXTERNAL FILE
+    template: html // I PREFER INLINE TEMPLATE, BUT YOU CAN USE SYSTEMJS PLUGIN .html! TO LOAD EXTERNAL FILE
 })
 export class AppCmp {
-    constructor( //inject dependency into constructor
-    //for basic factories (like angular 1.x) you must explict @Inject with string token
+    constructor( // inject dependency into constructor
+    // for basic factories (like angular 1.x) you must explict @Inject with string token
         @Inject("$window") private win: angular.IWindowService,
-    //for services defined as TS class decorated with @Injectable you can leverage TS reflect-metadata by Type without explicit @Inject
+    // for services defined as TS class decorated with @Injectable you can leverage TS reflect-metadata by Type without explicit @Inject
         qs: QuestionSvc
     ) {
         this.title = "ngMetadata";
         qs.getQuestions().then(q => this.question = q );
     }
-    
-    //property exposed to template via $ctrl (controllerAs default alias)
+
+    // property exposed to template via $ctrl (controllerAs default alias)
     public title: string;
     public question: string;
-    //method exposed to template via $ctrl (controllerAs default alias)
+    // method exposed to template via $ctrl (controllerAs default alias)
     public show(response) {
-        this.win.alert(`Thanks for your response:\n${response}!`);   
+        this.win.alert(`Thanks for your response:\n${response}!`);
     }
 }
--- app/services/question.service.ts
+++ app/services/question.service.ts
@@ -1,18 +1,18 @@
 import {Injectable, Inject} from "ng-metadata/core";
 
-@Injectable() //define a service as a class and decorate it with @Injectable to leverage TS reflect-metadata by Type
+@Injectable() // define a service as a class and decorate it with @Injectable to leverage TS reflect-metadata by Type
 export class QuestionSvc {
-    constructor( //inject dependency into constructor
-    //for basic factories (like angular 1.x) you must explict @Inject with string token
-        @Inject('$log') private log: angular.ILogService,
-        @Inject('$q') private $q: angular.IQService
-    ){
+    constructor( // inject dependency into constructor
+    // for basic factories (like angular 1.x) you must explict @Inject with string token
+        @Inject("$log") private log: angular.ILogService,
+        @Inject("$q") private $q: angular.IQService
+    ) {
         this._q = "What is your name";
     }
-    
+
     private _q: string;
     public getQuestions(): angular.IPromise<string> {
-        this.log.info("getQuestions:", this._q);    
+        this.log.info("getQuestions:", this._q);
         return this.$q.when(this._q);
     }
 }
